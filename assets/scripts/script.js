955aa9d0db31e403a2daa948074fc8cf6093d87a
JenaCarry <jeandaisu@gmail.com>
2025-06-07 16:06:45 -0300

    finalizando projeto

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -20,3 +20,15 @@ function addEventListenersToCards() {
 }
 
 document.addEventListener("DOMContentLoaded", addEventListenersToCards);
+
+function selectCarouselItem(selectedButtonElement) {
+    const selectedItem = selectedButtonElement.id;
+    const carousel = document.querySelector(".s-cards-carousel");
+    const rotateYDeg = -120 * (Number(selectedItem) - 1);
+    carousel.style.transform = `translateZ(-40vw) rotateY(${rotateYDeg}deg)`;
+    const activeButtonElement = document.querySelector(
+        ".s-controller__button--active"
+    );
+    activeButtonElement.classList.remove("s-controller__button--active");
+    selectedButtonElement.classList.add("s-controller__button--active");
+}
